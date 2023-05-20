5b08407ff350aecc547294ee333741ab7d04265d
Sergey <ssnovak@stud.kpfu.ru>
2023-05-20 18:27:04 +0300

    + methods for generate summary

---
+++ frequency_analysis.py
@@ -0,0 +1,30 @@
+from collections import Counter
+import heapq
+
+
+def main(text, num_sentences=5):
+    # Разбить текст на предложения
+    sentences = text.split('. ')
+
+    # Подсчитать частотность слов
+    words = [word for sentence in sentences for word in sentence.split()]
+    word_frequency = Counter(words)
+
+    # Оценить важность предложений на основе частотности слов
+    sentence_scores = {}
+    for sentence in sentences:
+        for word in sentence.split():
+            if word in word_frequency:
+                if len(sentence.split()) < 30:  # Учесть только предложения с меньшим числом слов
+                    if sentence not in sentence_scores:
+                        sentence_scores[sentence] = word_frequency[word]
+                    else:
+                        sentence_scores[sentence] += word_frequency[word]
+
+
+    # Выбрать наиболее важные предложения
+    summary_sentences = heapq.nlargest(num_sentences, sentence_scores, key=sentence_scores.get)
+
+    # Вернуть сокращенный текст
+    summary = '. '.join(summary_sentences)
+    return summary
